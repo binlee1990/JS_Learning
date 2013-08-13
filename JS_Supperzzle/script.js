da1b6ae6bf5c3d403e280aa9cc3323c5b8511a38
binlee1990 <Libin@Libin.cn.ibm.com>
2013-08-13 16:52:06 +0800

    Two color lumps can swap themselves.

--- JS_Supperzzle/script.js
+++ JS_Supperzzle/script.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
 			btnClass = '.'+btnClass;
 			return btnClass;
 		};
+		this.getColor = function(){
+			if(this.color === "red"){
+				return "#FF0000";
+			}else if(this.color === "green"){
+				return "#008800";
+			}else if(this.color === "blue"){
+				return "#0000FF";
+			}else if(this.color === "yellow"){
+				return "#E2BE22";
+			}
+		};
+		this.setColor = function(color) {
+			if(color === "#FF0000"){
+				this.color = "red";
+			}else if(color === "#008800"){
+				this.color = "green";
+			}else if(color === "#0000FF"){
+				this.color = "blue";
+			}else if(color === "#E2BE22"){
+				this.color = "yellow";
+			}
+		};
 	};
 	
 	var rows = 10;
@@ -40,13 +62,23 @@ $(document).ready(function() {
 		}
 	};
 	
-	var swap = function(btn1, btn2){
-		
-		
-	}
-	
 	var doRemove = function(btn1, btn2){
 		swap(btn1, btn2);
+		var canRemove = removeButtons();
+	}
+	
+	var removeButtons = function(){
+	
+	}
+	
+	var swap = function(btn1, btn2){
+		var btnColor1 = btn1.getColor();
+		var btnColor2 = btn2.getColor();
+		divButtonList[btn1.row*10+btn1.colum].setColor(btnColor2);
+		$(btn1.getClass()).css("background", btnColor2);
+		
+		divButtonList[btn2.row*10+btn2.colum].setColor(btnColor1);
+		$(btn2.getClass()).css("background", btnColor1);
 	}
 	
 	initDivButtons();
@@ -67,17 +99,25 @@ $(document).ready(function() {
 				selectedDiv = null;
 			}else{
 				selectedDiv2 = divButtonList[$(this).attr('class')];
-				var distance = Math.abs(selectedDiv2.row*10+selectedDiv2.colum-selectedDiv.row*10-selectedDiv.colum);
-				if(distance===1 || distance===10){
-					alert(selectedDiv.getClass());
-					doRemove(selectedDiv.getClass(), selectedDiv2.getClass());
+				var distance = selectedDiv2.row*10+selectedDiv2.colum-selectedDiv.row*10-selectedDiv.colum;
+				if(distance===1 || distance===10 || distance===-1 || distance===-10){
+					doRemove(selectedDiv, selectedDiv2);
+					var btnClass1 = selectedDiv.getClass();
+					$(btnClass1).css({border: "0px solid black"});
 					selectedDiv = null;
 					selectedDiv2 = null;
 					situation = 0;
+				}else {
+					var btnClass1 = selectedDiv.getClass();
+					$(btnClass1).css({border: "0px solid black"});
+					situation = $(this).attr('class');
+					$(this).css({border: "2px solid black"});
+					selectedDiv = divButtonList[situation];
 				}
 			}
 		}
 	});
+	
 	
 	/*$('div').mouseenter(function() {
 		$(this).css({border: "2px solid black"})
